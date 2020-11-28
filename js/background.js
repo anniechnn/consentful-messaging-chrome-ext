3acd1af2a545fa5713a626b9f23d7af491698f9c
trusttri <alwaysjane15@gmail.com>
2020-11-27 23:49:25 -0600

    fixed indentation

--- js/background.js
+++ js/background.js
@@ -1,13 +1,13 @@
 
 chrome.tabs.onUpdated.addListener(
-    function (tabId, changeInfo, tab) {
-        // read changeInfo data and do something with it
-        // like send the new url to contentscripts.js
-        if (changeInfo.url) {
-            chrome.tabs.sendMessage(tabId, {
-                message: 'confirmed',
-                url: changeInfo.url
-            })
-        }
+  function (tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it
+    // like send the new url to contentscripts.js
+    if (changeInfo.url) {
+      chrome.tabs.sendMessage(tabId, {
+        message: 'confirmed',
+        url: changeInfo.url
+      })
     }
+  }
 );
