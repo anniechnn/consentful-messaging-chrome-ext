8e048532818388ccc82dd67ee78bbc8f2d713359
trusttri <alwaysjane15@gmail.com>
2020-11-28 01:44:01 -0600

    fixed getting list of message senders; wrote code for making a basic request to server with the sender username

--- js/content.js
+++ js/content.js
@@ -1,25 +1,48 @@
+var URL_HEADER = "http://localhost:8000"
 
 function getMessageSenders() {
-  var messageTabElement = document.querySelector('[aria-label="Timeline: Messages"]');
-  var messages = messageTabElement.querySelectorAll('[data-testid="conversation"]');
-  var messageSenders = []
-  for(var i=0; i<messages.length; i++) {
-    var userCandidate = messages[i].querySelector('a')
-    var canId = userCandidate.href.replace("https://twitter.com/", '')
-    console.log(canId);
-    messageSenders.push(canId);
+  if (document.location.href == 'https://twitter.com/messages') {
+    var messageTabElement = document.querySelectorAll('[aria-label="Timeline: Messages"]')[0];
+    if(messageTabElement != null){
+      var messages = messageTabElement.querySelectorAll('[data-testid="conversation"]');
+      console.log(messages);
+      for(var i=0; i<messages.length; i++) {
+        var userCandidate = messages[i].querySelector('a')
+        var canId = userCandidate.href.replace("https://twitter.com/", '')
+        console.log(canId);
+        applyNetworkRules(canId);
+      }
+    }
   }
-  return messageSenders
 }
 
+
+function applyNetworkRules(sender){
+  // put placeholder for user temporarily
+  var url = URL_HEADER + "/author_network_rules?user=im__jane&sender=" + sender;
+    
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function(){
+      if (request.readyState == 4 && request.status == 200){
+          console.log('returned: ' + request.responseText)
+          // callback(request.responseText); 
+      }
+  };
+  console.log(url)
+  request.open('GET', url);
+  request.send();
+}
+
+// TODO: Weikun
 function checkProfile() {
   console.log('check profile');
 }
 
+// TODO: Weikun
 function getCurrentAccount() {
   return localStorage.getItem('accountName')
 }
-
+// TODO: Weikun
 function checkNotifUserId(document) {
   let container = document.querySelector(".stream");
   let items = container.querySelectorAll(".account-group");
@@ -29,48 +52,39 @@ function checkNotifUserId(document) {
   });
 }
 
-
+// TODO: Weikun
 function setLocalStorage() {
   profileStrangerString = (profileStrangerString) ? profileStrangerString : '{}'
   profileStrangers = JSON.parse(profileStrangerString)
   console.log(profileStrangers)
   localStorage.setItem('profileStrangers', JSON.stringify(profileStrangers))
 
-  flaggedTweetsString = (flaggedTweetsString) ? flaggedTweetsString : '{}'
-  flaggedTweets = JSON.parse(flaggedTweetsString)
-  console.log(flaggedTweets)
-  localStorage.setItem('flaggedTweets', JSON.stringify(flaggedTweets))
-
-  flaggedCredTweetsString = (flaggedCredTweetsString) ? flaggedCredTweetsString : '{}'
-  flaggedCredTweets = JSON.parse(flaggedCredTweetsString)
-  localStorage.setItem('flaggedCredTweets', JSON.stringify(flaggedCredTweets))
-
   followingListString = (followingListString) ? followingListString : '{}'
   followingList = JSON.parse(followingListString)
   console.log(followingList)
   console.log(followingList['im__jane'])
   localStorage.setItem('followingList', JSON.stringify(followingList))
-  
 }
 
 
 window.onload = function (ev) {
-  if (document.location.href == 'https://twitter.com/home') {
-    console.log("in twitter");
+  if (document.location.href == 'https://twitter.com/messages') {
+    console.log("in messages")
+  }
+  else if (document.location.href == 'https://twitter.com/home') {
+    console.log("in home");
     setLocalStorage();
   }
   else {
     console.log("not in twitter");
   }
-  var jsTimerForURLChange = setInterval(checkProfile, 2000);
+  setInterval(getMessageSenders, 2000);
 };
 
 
 window.onscroll = function (ev) {
-  getMessageSenders();
   accountName = getCurrentAccount();
   if (document.location.href == 'https://twitter.com/home') {
     console.log("scolling");
-    console.log(accountName);
   }
 };
