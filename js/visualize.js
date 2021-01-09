5d8ebf78a63308c005e3fc57832937a27a990ff5
trusttri <alwaysjane15@gmail.com>
2021-01-09 20:12:45 +0900

    fix visualize

--- js/visualize.js
+++ js/visualize.js
@@ -5,6 +5,7 @@ Code for visualizing results back on Twitter's interface
 
 function colorProfileBorder(senderDiv){
   let profileElement = senderDiv.querySelector('.css-1dbjc4n.r-sdzlij.r-1p0dtai.r-1mlwlqe.r-1d2f490.r-1udh08x.r-u8s1d.r-zchlnj.r-ipm5af.r-417010');
-  console.log(profileElement);
-  profileElement.style.border = '5px solid red';
+  if (profileElement != null){
+  	profileElement.style.border = '5px solid red';
+  }
 }
