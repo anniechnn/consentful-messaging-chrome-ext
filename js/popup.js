8a1c016892bb07596d92718afd3d81613ff7be39
NoradBorealis <71049406+NoradBorealis@users.noreply.github.com>
2021-04-09 09:44:30 -0500

    Add files via upload

---
+++ js/popup.js
@@ -0,0 +1,35 @@
+
+// Listener for login button
+
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("loaded")
+  console.log("initialized")
+
+  if (document.getElementById('Login') != null) {
+    document.getElementById('Login').addEventListener('click', oauthScript);
+    console.log("event listener added")
+  }
+});
+
+// Run oauth scirpt
+function oauthScript() {
+  console.log("clicked log in")
+  chrome.tabs.executeScript({
+    file: 'js/oauth.js'
+  });
+  window.location.href = chrome.extension.getURL("html/dashboard.html");
+  chrome.browserAction.setPopup({popup: "html/dashboard.html"});
+}
+
+
+// Listener for in twitter messages page
+chrome.runtime.onMessage.addListener(
+  function (request, sender, sendResponse) {
+    // listen for messages sent from background.js
+    if (request.message === 'confirmed') {
+      console.log(document.location.href)
+      if (document.location.href === "https://twitter.com/messages" || "http://twitter.com/messages") {
+        console.log("In messages!")
+      }
+    }
+  });
